8a895ed357be1450b4c056d3c148689e07348617
Andrew Straw <strawman@astraw.com>
2010-02-11 13:43:58 -0800

    add author and url to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 
 setup(name='txtraits',
+      author='Andrew Straw',
+      url='http://github.com/astraw/txtraits',
       version='0.0.1',
       py_modules=['txtraits'],
       license='MIT',
